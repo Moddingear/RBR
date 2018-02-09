687c87858196933cf3efef57a2b9d2743a6ba1f6
Hadrien <hadrienserralheiro@gmail.com>
2018-02-09 16:19:47 +0100

    Git LFS2 latest update

---
+++ Plugins/UE4GitPlugin-2.5-beta/Source/GitSourceControl/Private/GitSourceControlMenu.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2014-2018 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+//
+// Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+// or copy at http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ISourceControlProvider.h"
+
+class FToolBarBuilder;
+class FMenuBuilder;
+
+/** Git extension of the Source Control toolbar menu */
+class FGitSourceControlMenu
+{
+public:
+	void Register();
+	void Unregister();
+	
+	/** This functions will be bound to appropriate Command. */
+	void PushClicked();
+	void SyncClicked();
+	void RefreshClicked();
+
+private:
+	bool HaveRemoteUrl() const;
+
+	void				UnlinkSyncAndReloadPackages();
+	TArray<UPackage*>	UnlinkPackages(const TArray<FString>& InPackageNames);
+	void				ReloadPackages(TArray<UPackage*>& InLoadedPackages);
+
+	void AddMenuExtension(FMenuBuilder& Builder);
+
+	TSharedRef<class FExtender> OnExtendLevelEditorViewMenu(const TSharedRef<class FUICommandList> CommandList);
+
+	void DisplayInProgressNotification(const FText& InOperationInProgressString);
+	void RemoveInProgressNotification();
+	void DisplaySucessNotification(const FName& InOperationName);
+	void DisplayFailureNotification(const FName& InOperationName);
+
+private:
+	FDelegateHandle ViewMenuExtenderHandle;
+
+	/** Current source control operation from extended menu if any */
+	TWeakPtr<class SNotificationItem> OperationInProgressNotification;
+
+	/** Delegate called when a source control operation has completed */
+	void OnSourceControlOperationComplete(const FSourceControlOperationRef& InOperation, ECommandResult::Type InResult);
+};
